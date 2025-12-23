ceeca9f15034924421bf6986c8cd589da729ae16
sushil shai <sushil.079bca022@acem.edu.np>
2025-12-23 20:58:06 +0545

    railway

--- backend/settings.py
+++ backend/settings.py
@@ -27,6 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-j(rfmb)h)uo(&rdn^-!)(6b6+ukffqbzubu_iwmliu^w3y+o$u'
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://pasale-backend.onrender.com"
+]
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
